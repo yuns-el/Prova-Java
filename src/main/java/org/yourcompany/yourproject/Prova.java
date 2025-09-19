6bf0af074b5c51158b3df2a363ecc0978f4f0450
yunss-el <elhailoufiyounes342@gmail.com>
2025-09-19 11:42:20 +0200

    first commit

---
+++ src/main/java/org/yourcompany/yourproject/Prova.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author Younes
+ */
+public class Prova {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
